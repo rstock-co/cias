64dd75f8a6fe9d4798f8246ac33de6f5d2173278
Richard Stock <ricsto1@hotmail.com>
2023-06-11 17:43:57 -0600

    undo ux

--- src/components/allocationTable/view.js
+++ src/components/allocationTable/view.js
@@ -10,41 +10,15 @@ import {
     TableHead,
     TableRow,
     TableBody,
-    TableCell,
     DialogActions,
     Button,
     Box
 } from "@mui/material";
 
-import { styled } from '@mui/material/styles';
-import { tableCellClasses } from '@mui/material/TableCell';
 import { generateAllocationTableData } from "../../lib/functions/wallets";
 import { formatAmountDisplay } from "../../lib/functions/wallets";
 import { SortAllocationSelect } from "../selectInputs/sortAllocationSelect";
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
-        fontWeight: 'bold',
-        fontSize: 16,
-        fontFamily: 'Roboto, sans-serif'
-    },
-    [`&.${tableCellClasses.body}`]: {
-        fontSize: 14,
-        fontFamily: 'Roboto, sans-serif'
-    },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme, walletType }) => ({
-    '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-        border: 0,
-    },
-}));
+import { StyledTableCell, StyledTableRow } from "./styles";
 
 const AllocationTable = ({ tableData, dialogOpen, setDialogOpen, selectedWallet }) => {
     const [sortBy, setSortBy] = useState("# of contributions");
