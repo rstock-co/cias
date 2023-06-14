11e87fd8823d610e15f1e7636ae1865874bcd752
Richard Stock <ricsto1@hotmail.com>
2023-06-14 12:06:56 -0600

    fix styling of main wallet select

--- src/components/selectInputs/walletSelect.js
+++ src/components/selectInputs/walletSelect.js
@@ -1,41 +1,78 @@
-import { FormControl, OutlinedInput, InputLabel, MenuItem, Checkbox, ListItemText } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { StyledSelect } from './styles';
-import '@fontsource/inter';
+import { FormControl, OutlinedInput, InputLabel, MenuItem, Checkbox, ListItemText, Select } from '@mui/material';
+import { makeStyles, withStyles } from '@mui/styles';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
     menuItem: {
         fontFamily: 'Inter, sans-serif',
     },
-    checkbox: {
-        color: '#6DFAFE',
+});
+
+const CustomInputLabel = withStyles({
+    root: {
+        color: '#e6c347', // Set the label color
+        fontWeight: 'bold',
+        fontFamily: 'Inter, sans-serif',
+        fontSize: '17px',
+        letterSpacing: '0.75px',
+        '&.Mui-focused': {
+            color: '#e6c347',
+            fontWeight: 'bold',
+        },
     },
-}));
+})(InputLabel);
+
+const CustomOutlinedInput = withStyles({
+    root: {
+        color: '#6DFAFE',
+        height: '50px',
+        '& .MuiOutlinedInput-notchedOutline': {
+            borderColor: '#096B78', // Set the border color
+        },
+        '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: '#096B78', // Keep the border color on hover
+        },
+        '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            borderColor: '#096B78', // Keep the border color when focused
+        },
+    },
+})(OutlinedInput);
+
+const CustomSelect = withStyles({
+    root: {
+        height: '50px',
+        '& .MuiSelect-icon': {
+            color: '#6DFAFE', // Set the select dropdown arrow color
+        },
+    },
+    "& .MuiSelect-select": {
+        backgroundColor: "#011D24",
+        color: "#6DFAFE",
+    },
+})(Select);
 
 export const WalletSelect = ({ wallets, selectedWallets, handleChange }) => {
     const classes = useStyles();
     return (
-        <FormControl sx={{ width: '450px' }}>
-            <InputLabel id="demo-multiple-checkbox-label" className='MuiInputLabel-root'>Wallet</InputLabel>
-            <StyledSelect
-                labelId="demo-multiple-checkbox-label"
-                id="demo-multiple-checkbox"
-                multiple
-                value={selectedWallets.map(wallet => wallet.name)}
-                onChange={handleChange}
-                input={<OutlinedInput label="Wallet" classes={{ root: 'MuiOutlinedInput-root', input: 'MuiOutlinedInput-input custom-input' }} />}
-                renderValue={(selected) => selected.join(', ')}
-            >
-                <MenuItem disabled value="" className={classes.menuItem}>
-                    <em>None</em>
-                </MenuItem>
-                {wallets.map((wallet) => (
-                    <MenuItem key={wallet.name} value={wallet.name} className={classes.menuItem}>
-                        <Checkbox checked={selectedWallets.some(sw => sw.name === wallet.name)} className={classes.checkbox} />
-                        <ListItemText primary={wallet.name} />
-                    </MenuItem>
-                ))}
-            </StyledSelect>
-        </FormControl>
+        <div>
+            <FormControl sx={{ m: 1, width: 450 }}>
+                <CustomInputLabel id="demo-multiple-checkbox-label">Select Wallet</CustomInputLabel>
+                <CustomSelect
+                    labelId="demo-multiple-checkbox-label"
+                    id="demo-multiple-checkbox"
+                    multiple
+                    value={selectedWallets.map(wallet => wallet.name)}
+                    onChange={handleChange}
+                    input={<CustomOutlinedInput label="Select Wallet" />}
+                    renderValue={(selected) => selected.join(', ')}
+                >
+                    {wallets.map(wallet => (
+                        <MenuItem key={wallet.name} value={wallet.name} className={classes.menuItem} >
+                            <Checkbox checked={selectedWallets.some(sw => sw.name === wallet.name)} />
+                            <ListItemText primary={wallet.name} />
+                        </MenuItem>
+                    ))}
+                </CustomSelect>
+            </FormControl>
+        </div>
     );
 }
