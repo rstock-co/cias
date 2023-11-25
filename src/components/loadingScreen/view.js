4ae9bde589b4a9bc1404a81931883df1bfd8c37e
Jean-Paul S. Boodhoo <63258+developwithpassion@users.noreply.github.com>
2023-11-25 10:00:52 -0700

    chore: break off small template for CoinTemplate

--- src/components/loadingScreen/view.js
+++ src/components/loadingScreen/view.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { DialogContent, Box } from '@mui/material';
-import { StyledDialog, StyledBox, StyledTypography } from './styles';
 import "@fontsource/inter-tight";
 import "./logos.css";
+import { Box, DialogContent } from '@mui/material';
+import { StyledBox, StyledDialog, StyledTypography } from './styles';
+import React from 'react';
 
 const logos = {
   arb: 'https://i.imgur.com/3kJricG.png',
@@ -10,6 +10,19 @@ const logos = {
   bsc: 'https://i.imgur.com/a5V7FFD.png',
 };
 
+const CoinTemplate = ([coinKey, { name, chain, loading, txns }]) => (
+  <Box key={coinKey} sx={{ display: 'flex', alignItems: 'center', marginBottom: 2 }}>
+    <StyledTypography>
+      {loading ? `Loading transactions for ${name}...  ` : `Loaded ${txns} transactions for ${name}  `}
+    </StyledTypography>
+    <img 
+      src={logos[chain]} 
+      className={`${loading ? "spinner" : ""} spinner-${chain.toLowerCase()}`} 
+      alt={`${name} logo`} 
+      style={{ width: '40px', height: '40px', marginLeft: '10px' }} 
+    />
+  </Box>
+)
 
 const LoadingScreen = ({ stableCoins, open = false }) => {
   console.log("StableCoins from loading screen: ", stableCoins)
@@ -19,19 +32,7 @@ const LoadingScreen = ({ stableCoins, open = false }) => {
     >
       <DialogContent>
         <StyledBox>
-          {Object.entries(stableCoins).map(([coinKey, { name, chain, loading, txns }]) => (
-            <Box key={coinKey} sx={{ display: 'flex', alignItems: 'center', marginBottom: 2 }}>
-              <StyledTypography>
-                {loading ? `Loading transactions for ${name}...  ` : `Loaded ${txns} transactions for ${name}  `}
-              </StyledTypography>
-              <img 
-                src={logos[chain]} 
-                className={`${loading ? "spinner" : ""} spinner-${chain.toLowerCase()}`} 
-                alt={`${name} logo`} 
-                style={{ width: '40px', height: '40px', marginLeft: '10px' }} 
-            />
-            </Box>
-          ))}
+          {Object.entries(stableCoins).map(CoinTemplate)}
         </StyledBox>
       </DialogContent>
     </StyledDialog>
