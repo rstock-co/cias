968a53aca191ebe90f921753de067c1aa4fb37dd
Jean-Paul S. Boodhoo <63258+developwithpassion@users.noreply.github.com>
2023-11-25 13:55:23 -0700

    chore: break up allocation table further

--- src/components/allocationTable/view.js
+++ src/components/allocationTable/view.js
@@ -9,6 +9,67 @@ import { CustomColorSwitch } from "../../elements/toggles/coloredToggle";
 import { SortAllocationSelect } from "../../elements/dropdowns/sortAllocationSelect";
 import { WalletSummary } from "../../elements/templates/tables";
 
+const ShowTotalsRowTemplate = ({ showHeaderRow, handleToggleHeaderRow }) => (
+    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', mr: 2 }}>
+        <Typography component="div" sx={{ fontFamily: 'Inter Tight' }}>
+            Show Totals Row
+        </Typography>
+        <CustomColorSwitch
+            checked={showHeaderRow}
+            onChange={handleToggleHeaderRow}
+            inputProps={{ 'aria-label': 'Toggle Header Row' }}
+        />
+    </Box>
+)
+
+const ShowMemberNameTemplate = ({ showMemberName, handleToggleMemberName }) => (
+    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', mr: 2 }}>
+        <Typography component="div" sx={{ fontFamily: 'Inter Tight' }}>
+            Show Member Name
+        </Typography>
+        <CustomColorSwitch
+            checked={showMemberName}
+            onChange={handleToggleMemberName}
+            color="primary"
+            inputProps={{ 'aria-label': 'Toggle Member Name' }}
+        />
+    </Box>
+)
+
+const SortAllocationSelectTemplate = ({ sortBy, handleSortByChange }) => (
+    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', mr: 2 }}>
+        <SortAllocationSelect sortBy={sortBy} handleSortByChange={handleSortByChange} />
+    </Box>
+)
+
+const AdjustedNetTotalTemplate = ({adjustedNetTotal, handleAdjustedNetTotalChange }) => (
+    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', mr: 2 }}>
+        <FormControl fullWidth sx={{ m: 1 }}>
+            <InputLabel 
+                htmlFor="outlined-adornment-amount" 
+                style={{ fontWeight: 'bold', color: '#097c8f' }}
+            >
+                Adjusted Net Total
+            </InputLabel>
+            <OutlinedInput
+                id="outlined-adornment-amount"
+                variant="outlined"
+                size="small"
+                type="number"
+                value={adjustedNetTotal}
+                onChange={handleAdjustedNetTotalChange}
+                startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                label="Adjust Total Net Am"
+                placeholder="Enter adjusted net"
+                sx={{ margin: "none", maxWidth: 200, fontFamily: 'Inter Tight, sans-serif' }}
+                InputLabelProps={{
+                    style: { fontWeight: 'bold', color: '#097c8f' },
+                }}
+            />
+        </FormControl>
+    </Box>
+)
+
 const HeaderTemplate = ({
     savedTableId,
     showHeaderRow, 
@@ -32,62 +93,13 @@ const HeaderTemplate = ({
             </Box>
         </Box>
         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', ml: 2 }}>
-            {/* Show Totals Row */}
-            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', mr: 2 }}>
-                <Typography component="div" sx={{ fontFamily: 'Inter Tight' }}>
-                    Show Totals Row
-                </Typography>
-                <CustomColorSwitch
-                    checked={showHeaderRow}
-                    onChange={handleToggleHeaderRow}
-                    inputProps={{ 'aria-label': 'Toggle Header Row' }}
-                />
-            </Box>
+            {ShowTotalsRowTemplate({ showHeaderRow, handleToggleHeaderRow })}
 
-            {/* Show Member Name */}
-            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', mr: 2 }}>
-                <Typography component="div" sx={{ fontFamily: 'Inter Tight' }}>
-                    Show Member Name
-                </Typography>
-                <CustomColorSwitch
-                    checked={showMemberName}
-                    onChange={handleToggleMemberName}
-                    color="primary"
-                    inputProps={{ 'aria-label': 'Toggle Member Name' }}
-                />
-            </Box>
+            {ShowMemberNameTemplate({showMemberName, handleToggleMemberName})}
 
-            {/* Sort Allocation Select */}
-            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', mr: 2 }}>
-                <SortAllocationSelect sortBy={sortBy} handleSortByChange={handleSortByChange} />
-            </Box>
+            {SortAllocationSelectTemplate({ sortBy, handleSortByChange })}
 
-            {/* Adjusted Net Total */}
-            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', mr: 2 }}>
-                <FormControl fullWidth sx={{ m: 1 }}>
-                    <InputLabel 
-                        htmlFor="outlined-adornment-amount" 
-                        style={{ fontWeight: 'bold', color: '#097c8f' }}
-                    >
-                        Adjusted Net Total
-                    </InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-amount"
-                        variant="outlined"
-                        size="small"
-                        type="number"
-                        value={adjustedNetTotal}
-                        onChange={handleAdjustedNetTotalChange}
-                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                        label="Adjust Total Net Am"
-                        placeholder="Enter adjusted net"
-                        sx={{ margin: "none", maxWidth: 200, fontFamily: 'Inter Tight, sans-serif' }}
-                        InputLabelProps={{
-                            style: { fontWeight: 'bold', color: '#097c8f' },
-                        }}
-                    />
-                </FormControl>
-            </Box>
+            {AdjustedNetTotalTemplate({ adjustedNetTotal, handleAdjustedNetTotalChange })}
         </Box>
     </Box>
 )
