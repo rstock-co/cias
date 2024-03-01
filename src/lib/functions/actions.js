0d83bddd88f668c12c834f3993071071292c9b00
Richard Stock <rstock.co@gmail.com>
2024-02-29 22:28:33 -0700

    small fix

--- src/lib/functions/actions.js
+++ src/lib/functions/actions.js
@@ -160,7 +160,7 @@ export const copyDistributionToClipboard = async (sortedAllocationTableData, tot
   // Prepare title and summary rows for the team and charity fees
   const titleString = `${formattedWalletName}\n\n`;
   const headerString = "Wallet Address\tTokens\tShare (%)\n";
-  const summaryString = `Total # of tokens:\t${totalTokens}\nTeam fee:\t${teamFee.toFixed(4)}\nCharity fee:\t${charityFee.toFixed(4)}\n\nTotal # of tokens distributed:\t${tokensToDistribute.toFixed(4)}\n\n`;
+  const summaryString = `Total # of tokens:\t${Number(totalTokens).toFixed(2)}\nTeam fee:\t${teamFee.toFixed(4)}\nCharity fee:\t${charityFee.toFixed(4)}\n\nTotal # of tokens distributed:\t${tokensToDistribute.toFixed(4)}\n\n`;
 
   // Serialize tableData to a string format suitable for spreadsheets
   const tableString = sortedAllocationTableData.map(row => {
